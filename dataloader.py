6051d513b9076bf8e1343699154520ff9b0ac83a
mi-song <misong023@khu.ac.kr>
2023-02-14 22:11:47 +0900

    [Add, Fix] Add City csv file, Fix dataloader

--- dataloader.py
+++ dataloader.py
@@ -4,6 +4,8 @@ import networkx as nx
 from scipy import sparse
 
 def data_loader(city_name):
+    city_name = (city_name.lower()).replace(" ", "")
+
     node_file_path = "./datasets/cities/norm/" + city_name + "_node_norm_highway_one.csv"
     edge_file_path = "./datasets/cities/norm/" + city_name + "_edge_norm_ver1_highway.csv"
 
@@ -11,7 +13,11 @@ def data_loader(city_name):
     node_id = node['id']
     node_lon = node['lon']
     node_lat = node['lat']
-    node_highway = node['highway']
+    node_highway1 = node['highway1']
+    node_highway2 = node['highway2']
+    node_highway3 = node['highway3']
+    node_highway4 = node['highway4']
+    node_highway5 = node['highway5']
 
     edge = pd.read_csv(edge_file_path)
     edge_src = edge['src']
@@ -19,13 +25,54 @@ def data_loader(city_name):
 
     G = nx.Graph()
 
+    node_1 = np.array([])
+    node_2 = np.array([])
+    node_3 = np.array([])
+    node_4 = np.array([])
+    node_5 = np.array([])
+
     # Node
-    node_list = []
 
-    for i in range(len(node_lon)):
-        node_list.append([node_lon[i], node_lat[i]])
+    node_list1 = []
+    node_list2 = []
+    node_list3 = []
+    node_list4 = []
+    node_list5 = []
 
-    x = np.array(node_list)
+
+    for i in range(1, len(node_lon)):
+        if node_highway1[i] != 0:
+            node_arr_1_1 = np.array([[node_lon[i], node_lat[i], 0., 0., 0.],
+                                     [node_highway1[i], node_highway2[i], node_highway3[i], node_highway4[i],
+                                      node_highway5[i]]])
+            node_list1.append(node_arr_1_1)
+        if node_highway2[i] != 0:
+            node_arr_1_2 = np.array([[node_lon[i], node_lat[i], 0., 0., 0.],
+                                     [node_highway1[i], node_highway2[i], node_highway3[i], node_highway4[i],
+                                      node_highway5[i]]])
+            node_list2.append(node_arr_1_2)
+        if node_highway3[i] != 0:
+            node_arr_1_3 = np.array([[node_lon[i], node_lat[i], 0., 0., 0.],
+                                     [node_highway1[i], node_highway2[i], node_highway3[i], node_highway4[i],
+                                      node_highway5[i]]])
+            node_list3.append(node_arr_1_3)
+        if node_highway4[i] != 0:
+            node_arr_1_4 = np.array([[node_lon[i], node_lat[i], 0., 0., 0.],
+                                     [node_highway1[i], node_highway2[i], node_highway3[i], node_highway4[i],
+                                      node_highway5[i]]])
+            node_list4.append(node_arr_1_4)
+        if node_highway5[i] != 0:
+            node_arr_1_5 = np.array([[node_lon[i], node_lat[i], 0., 0., 0.],
+                                     [node_highway1[i], node_highway2[i], node_highway3[i], node_highway4[i],
+                                      node_highway5[i]]])
+            node_list5.append(node_arr_1_5)
+
+    x1 = np.array(node_list1)
+    x2 = np.array(node_list2)
+    x3 = np.array(node_list3)
+    x4 = np.array(node_list4)
+    x5 = np.array(node_list5)
+
 
     # Edge
     for i in range(len(edge_src)):
@@ -34,4 +81,4 @@ def data_loader(city_name):
     A = nx.adjacency_matrix(G)
     A = sparse.lil_matrix(A)
 
-    return A, x
+    return A, x1, x2, x3, x4, x5
--- test_dl.py
+++ test_dl.py
@@ -1,6 +1,22 @@
 from dataloader import data_loader
 
-A, x = data_loader("atlanta")
+# available cities
+# madrid, munchen, minneapolis, saintpaul, richmond, littlerock, portland, washington, paris, firenze, atlanta, frankfurtammain
+
+A, x1, x2, x3, x4, x5 = data_loader("minneapolis")
+
+# type(A) : <class 'scipy.sparse._lil.lil_matrix'>
+# type(x1) : <class 'numpy.ndarray'>
+# type(x1[i]) : <class 'numpy.ndarray'>
+
+"""
+example of x3 
+[[11.2292767 43.7892378  0.         0.         0.       ] -> x, y, 0, 0, 0
+  [ 4.         0.         1.         0.         0.       ]]] -> one-hot encoding (motorway+trunk+primary/+secondary/+tertiary/+unclassified/+residential)
+"""
 
 print(A)
-print(x)
+# print(type(A))
+print(x3)
+
+
